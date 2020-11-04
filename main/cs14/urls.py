96da46dfc9ec505a9ec2cad4c06d0fd3dd54f606
Calum MacPhee <2388714m@student.gla.ac.uk>
2020-11-04 17:02:30 +0000

    Basic login working

--- main/cs14/urls.py
+++ main/cs14/urls.py
@@ -6,4 +6,5 @@ app_name = 'cs14'
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('register/', views.register, name='register'),
+	path('login/', views.loginPage, name='login')
 ]
--- main/cs14/views.py
+++ main/cs14/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from cs14.forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 def index(request):
     return render(request, 'cs14/index.html')
@@ -12,6 +14,23 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('cs14:login')
         
     context = {'form': form}
     return render(request, 'cs14/register.html', context)
+
+def loginPage(request):
+
+    if request.method == 'POST':
+       username = request.POST.get('username')
+       password = request.POST.get('password')
+       user = authenticate(request, username=username, password=password)
+
+       if user is not None:
+           login(request, user)
+           return redirect('cs14:index')
+
+    context = {}
+    return render(request, 'cs14/login.html')
