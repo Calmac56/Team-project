3142c0379447f87c61abbbb675abf49f730359a3
sillars <2404079s@student.gla.ac.uk>
2020-11-29 01:34:02 +0000

    Updated models for three types of user

--- main/cs14/models.py
+++ main/cs14/models.py
@@ -1,40 +1,31 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User 
 
 # Create your models here.
-
-class Candidate(models.Model):
+class GeneralUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length = 255) 
+    is_candidate = models.BooleanField('candidate status', default=False)
+    is_reviewer = models.BooleanField('reviewer status', default=False)
+    is_admin = models.BooleanField('admin status', default=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length = 255)
     last_name = models.CharField(max_length = 255)
     userID = models.IntegerField(primary_key=True)
-    admin = False
-    #email and password in user model?
+    #email and password in User model
 
     def __str__(self):
         return self.user.username
 
-
-class Admin(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length = 255) 
-    last_name = models.CharField(max_length = 255)
-    userID = models.IntegerField(primary_key=True)
-    admin = True
-    #email and password in user model?
-
-    def __str__(self):
-        return self.user.username
 
 class Task(models.Model):
     taskID = models.IntegerField(primary_key=True)
     description = models.CharField(max_length = 4095)
     testcases = models.CharField(max_length = 255)
     expectedout = models.CharField(max_length = 255)
-    creator = models.ForeignKey(Admin, on_delete=models.CASCADE)
+    creator = models.ForeignKey(GeneralUser, on_delete=models.CASCADE)
 
 class Results(models.Model):
-    userID =  models.ForeignKey(Candidate, on_delete=models.CASCADE)
+    userID =  models.ForeignKey(GeneralUser, on_delete=models.CASCADE)
     taskID = models.ForeignKey(Task, on_delete=models.CASCADE)
     tests_passed = models.IntegerField()
     tests_failed = models.IntegerField()
