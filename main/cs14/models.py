ddee4a99bfac5533ed036aec1dedab6ebcf1f689
sillars <2404079s@student.gla.ac.uk>
2020-12-01 16:41:42 +0000

    Changed models to work with other update

--- main/cs14/models.py
+++ main/cs14/models.py
@@ -2,17 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User 
 
 # Create your models here.
-class GeneralUser(models.Model):
+class Admin (models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    is_candidate = models.BooleanField('candidate status', default=False)
-    is_reviewer = models.BooleanField('reviewer status', default=False)
-    is_admin = models.BooleanField('admin status', default=False)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length = 255)
-    last_name = models.CharField(max_length = 255)
-    userID = models.IntegerField(primary_key=True)
-    #email and password in User model
+    def __str__(self):
+        return self.user.username
 
+class Candidate (models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.user.username
+
+class Reviewer (models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.user.username
 
