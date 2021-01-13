8917e8ba32d3606643a99fa68fdeb2b625ec935c
Calmac56 <60009981+Calmac56@users.noreply.github.com>
2021-01-13 15:51:51 +0000

    Created candidates results page

--- main/cs14/context_processors.py
+++ main/cs14/context_processors.py
@@ -1,4 +1,4 @@
-from cs14.models import Admin, Reviewer
+from cs14.models import Admin, Reviewer, Candidate
 
 def isAdmin(request):
     auser = None
@@ -27,3 +27,16 @@ def isReviewer(request):
         return {'isReviewer': False}
     else:
         return {'isReviewer': True}
+
+def isCandidate(request):
+    auser = None
+    try:
+        if request.user.is_authenticated:
+            auser = Candidate.objects.get(user=request.user)
+    except Candidate.DoesNotExist:
+        auser = None
+    
+    if auser == None:
+        return {'isCandidate': False}
+    else:
+        return {'isCandidate': True}
