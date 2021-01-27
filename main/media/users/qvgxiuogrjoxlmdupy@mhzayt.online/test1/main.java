11f7a6e3887e8fbb8eceb268e17d30aea9bce324
Mark Harris <mark_harris77@hotmail.com>
2021-01-27 11:49:29 +0000

    Fix java compilation

--- main/media/users/qvgxiuogrjoxlmdupy@mhzayt.online/test1/main.java
+++ main/media/users/qvgxiuogrjoxlmdupy@mhzayt.online/test1/main.java
@@ -1,3 +1,7 @@
-import sys
-l = sys.stdin.read()
-print(l)
+class main{
+    public static void main(String[] args){
+        for(int i = 0; i < 5; i++){
+            System.out.println(i + 2);
+        }
+    }
+}
---
+++ main/temp.py
@@ -0,0 +1,6 @@
+for i in range(3):
+    print(1)
+
+    for j in range(3):
+        print(1, end="")
+        print()
