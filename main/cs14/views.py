98b29d816e21c26a9e39f77f32344682e467447f
Ka Hung IP <2431121i@student.gla.ac.uk>
2021-02-23 19:04:25 +0800

    fixed import error

--- main/cs14/views.py
+++ main/cs14/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from static.python.compile import *
 from django.contrib.auth.forms import UserCreationForm
-from .forms import CreateUserForm, CreateLoginLink, ProfileForm
+from .forms import CreateUserForm, CreateLoginLink, Profile
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
