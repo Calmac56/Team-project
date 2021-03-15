4814d34c54d44307b67c88532fdcdea97a212679
Calmac56 <60009981+Calmac56@users.noreply.github.com>
2021-03-15 21:24:29 +0000

    Added more tests to results page

--- main/cs14/reviewtests.py
+++ main/cs14/reviewtests.py
@@ -15,28 +15,65 @@ class resultsTest(TestCase):
         test_user = User.objects.create_user(username='testuser')
         test_user.set_password('testpassword')
         test_user.save()
+        test_user2 = User.objects.create_user(username='testuser2')
+        test_user2.set_password('testpassword')
+        test_user2.save()
         test_admin = User.objects.create_user(username='testAdmin')
-        test_user.set_password('testpassword')
-        test_user.save()
+        test_admin.set_password('testpassword')
+        test_admin.save()
+        test_reviewer = User.objects.create_user(username='testReviewer')
+        test_reviewer.set_password('testpassword')
+        test_reviewer.save()
+        rev = Reviewer.objects.create(user=test_reviewer)
+        rev.save()
         Admin.objects.create(user=test_admin)
         theadmin = Admin.objects.get(user=test_admin)
         Task.objects.create(taskID=1, description="testTask", testcases="0123", expectedout="0123",creator=theadmin, time=1000)
+        Task.objects.create(taskID=2, description="testTask2", testcases="1234", expectedout="1234",creator=theadmin, time=1000)
         testTask = Task.objects.get(taskID=1)
+        testTask2 = Task.objects.get(taskID=2)
         Candidate.objects.create(user=test_user)
         candidate = Candidate.objects.get(user=test_user)
+        cand = Candidate.objects.create(user=test_user2)
+        candidate2 = Candidate.objects.get(user=test_user2)
         Results.objects.create(userID=candidate, passpercentage =80, taskID=testTask, tests_passed=4, tests_failed=1, timetaken=1, complexity="test", language="java")
-        self.c = Client()
-        self.user= self.c.login(username='testuser', password='testpassword')
-    def test_candidate_result(self):
-       
+        Results.objects.create(userID=candidate2, passpercentage=80,taskID=testTask, tests_passed=4, tests_failed=1, timetaken=1, complexity="test", language="java")
+        Results.objects.create(userID=candidate2, passpercentage=60,taskID=testTask2, tests_passed=3, tests_failed=2, timetaken=1, complexity="test", language="java")
         
-    
-        candidate = Candidate.objects.get(user=self.user)
-        response = self.c.get(reverse('cs14:cresults'))
-        testuser = response.wsgi_request.user
+    def test_candidate_result(self):
+        c = Client()
+        user= c.login(username='testuser', password='testpassword')
+        candidate = Candidate.objects.get(user=user)
+        response = c.get(reverse('cs14:cresults'))
+        
       
         self.assertEquals(list(response.context['results'].values_list()), list(Results.objects.filter(userID=candidate).values_list()))
+        print("Candidate result ok")
+    
+    def test_reviewer_result(self):
 
+        c = Client()
+        user = c.login(username='testReviewer', password='testpassword')
+        candidate = Candidate.objects.get(user=User.objects.get(username='testuser'))
+        response = c.get('/cs14/results/', {'search':'testuser'})
+        self.assertEquals(list(response.context['results'].values_list()), list(Results.objects.filter(userID=candidate).values_list()))
+        print("Reviewer results ok")
+    
+    def test_candidate_numresult(self):
+        c = Client()
+        user= c.login(username='testuser2', password='testpassword')
+        candidate = Candidate.objects.get(user=user)
+        response = c.get(reverse('cs14:cresults'))
+        self.assertEqual(len(response.context['results']), 2)
+        print("Number of candidate results ok")
+       
+    def test_reviewer_numresult(self):
+        c = Client()
+        user = c.login(username='testReviewer', password='testpassword')
+        candidate = Candidate.objects.get(user=User.objects.get(username='testuser'))
+        response = c.get('/cs14/results/', {'search':'testuser2'})
+        self.assertEquals(len(response.context['results']), 2)
+        print("Number of reviewer results ok")
  
 
 class reviewTest(TestCase):
@@ -71,6 +108,7 @@ class reviewTest(TestCase):
         response = self.c.get(reverse('cs14:creview', kwargs={'id':1}))
         code = response.context["code"]
         self.assertEqual(['print("Hello world from calum")'], code)
+        print("Intial review page ok")
 
     def test_run(self):
       
@@ -80,12 +118,14 @@ class reviewTest(TestCase):
         contains = 'Test Case 1:' in theresp
     
         self.assertTrue(contains)
+        print("Code execution on review page ok")
 
     def test_timeline(self):
         response = self.c.get(reverse('cs14:creview', kwargs={'id':1}))
         theresp = self.c.post('/cs14/rhistory', {'number':0, 'taskID': 1})
         theresp = theresp.content.decode('ascii')
         self.assertEqual('print("Hello world")', theresp)
+        print("Timeline ok")
 
     @classmethod
     def tearDownClass(self):
