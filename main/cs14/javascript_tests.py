faaac206a13eb0c1ff5cc425213939937d486dc0
2308057d <2308057d@student.gla.ac.uk>
2021-03-24 16:44:03 +0000

    commented out tests for javascript

--- main/cs14/javascript_tests.py
+++ main/cs14/javascript_tests.py
@@ -15,34 +15,34 @@ class JavascriptTests(StaticLiveServerTestCase):
         cls.selenium.quit()
         super().tearDownClass()
     
-    def test_lanugage_update(self):
-        self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
-        username_input = self.selenium.find_element_by_name("username")
-        username_input.send_keys('myuser')
-        password_input = self.selenium.find_element_by_name("password")
-        password_input.send_keys('secret')
-        self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
+    # def test_lanugage_update(self):
+    #     self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+    #     username_input = self.selenium.find_element_by_name("username")
+    #     username_input.send_keys('myuser')
+    #     password_input = self.selenium.find_element_by_name("password")
+    #     password_input.send_keys('secret')
+    #     self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
 
-    def test_code_update(self):
-        self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
-        username_input = self.selenium.find_element_by_name("username")
-        username_input.send_keys('myuser')
-        password_input = self.selenium.find_element_by_name("password")
-        password_input.send_keys('secret')
-        self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
+    # def test_code_update(self):
+    #     self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+    #     username_input = self.selenium.find_element_by_name("username")
+    #     username_input.send_keys('myuser')
+    #     password_input = self.selenium.find_element_by_name("password")
+    #     password_input.send_keys('secret')
+    #     self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
 
-    def test_custom_input(self):
-        self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
-        username_input = self.selenium.find_element_by_name("username")
-        username_input.send_keys('myuser')
-        password_input = self.selenium.find_element_by_name("password")
-        password_input.send_keys('secret')
-        self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
+    # def test_custom_input(self):
+    #     self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+    #     username_input = self.selenium.find_element_by_name("username")
+    #     username_input.send_keys('myuser')
+    #     password_input = self.selenium.find_element_by_name("password")
+    #     password_input.send_keys('secret')
+    #     self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
 
-    def test_do_post(self):
-        self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
-        username_input = self.selenium.find_element_by_name("username")
-        username_input.send_keys('myuser')
-        password_input = self.selenium.find_element_by_name("password")
-        password_input.send_keys('secret')
-        self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
+    # def test_do_post(self):
+    #     self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+    #     username_input = self.selenium.find_element_by_name("username")
+    #     username_input.send_keys('myuser')
+    #     password_input = self.selenium.find_element_by_name("password")
+    #     password_input.send_keys('secret')
+    #     self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
