f38a597018fbcf191434c1721fce11cf29316077
Ka Hung IP <2431121i@student.gla.ac.uk>
2021-03-24 18:44:32 +0800

    Finish the profile test and merge

--- main/cs14/profile_test.py
+++ main/cs14/profile_test.py
@@ -2,37 +2,30 @@ from django.test import TestCase
 from django.test import Client
 from django.urls import reverse
 from django.contrib.auth.models import User
+from .models import *
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 class test_ProfilePage(TestCase):
-
-    def setUpTestData(cls):
-        user.objects.create(first_name='Harry', last_name='Potter', email ="Testing@gmail.com")
+    #email = "Testing@gmail.com"
+    #first_name='Harry'
+    #last_name='Potter'
 
     def test_first_name_label(self):
-        user = user.objects.get(id=1)
+        user = User.objects.create(first_name='Harry', last_name='Potter', email = "Testing@gmail.com")  
         field_label = user._meta.get_field('first_name').verbose_name
         self.assertEqual(field_label,'first name')
-        print("First name label OK")
+        print("Last name label OK")
     
     def test_last_name_label(self):
-        user = user.objects.get(id=1)
+        user = User.objects.create(first_name='Harry', last_name='Potter', email = "Testing@gmail.com")  
         field_label = user._meta.get_field('last_name').verbose_name
         self.assertEqual(field_label,'last name')
         print("Last name label OK")
 
     def test_email(self):
-        user = user.objects.get(id=1)
-        field_label = user._meta.get_field('last_name').verbose_name
-        self.assertEqual(field_label,'last name')
+        user = User.objects.create(first_name='Harry', last_name='Potter', email = "Testing@gmail.com")  
+        field_label = user._meta.get_field('email').verbose_name
+        self.assertEqual(field_label,'email address')
         print("Email OK")
-
-    def test_object_name_is_last_name_comma_first_name(self):
-        user = user.objects.get(id=1)
-        expected_object_name = f'{user.last_name}, {user.first_name}'
-        self.assertEqual(expected_object_name, str(user))
-        print("Name is last name comma first name")
-
-    def test_ProfilePage_url(self):
-        self.assertTemplateUsed(self.response, 'cs14/profile.html')
-        print("Profile page access success")
+    
     
